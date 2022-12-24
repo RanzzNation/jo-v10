d670a9f6f438074c1bcfffb35fa33dd3ee88b99f
RanzzNation <116924759+RanzzNation@users.noreply.github.com>
2022-12-24 14:24:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'AHMADKECE'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6289697155781','6289697155781','6289697155781']
+global.premium = ['6289697155781']
+global.packname = 'Base Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
